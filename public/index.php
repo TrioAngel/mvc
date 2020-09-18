ab19ff7364373e2b01b0e241e8e4572614e1e0dc
Angela Babayan <angela.babayan.00@gmail.com>
2020-09-18 13:18:36 +0400

    adding Router class and .htaccess

--- public/index.php
+++ public/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "Requested URL: " . $_SERVER['QUERY_STRING'];
+//echo "Requested URL: " . $_SERVER['QUERY_STRING'];
+
+//Routing
+require '../Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
