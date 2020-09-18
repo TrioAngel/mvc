37aadd019af82a1a335728b61229d6bf81bada1a
Angela Babayan <angela.babayan.00@gmail.com>
2020-09-18 13:48:43 +0400

    adding route table and match function

--- public/index.php
+++ public/index.php
@@ -7,4 +7,25 @@ require '../Core/Router.php';
 
 $router = new Router();
 
-echo get_class($router);
+//echo get_class($router);
+
+//Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+//Display the routing table
+/*echo '<pre>';
+print_r($router->getRoutes());
+echo '</pre>';*/
+
+//Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if($router->match($url)){
+  echo '<pre>';
+  print_r($router->getParams());
+  echo '</pre>';
+} else {
+  echo "no route found for URL '$url'";
+}
